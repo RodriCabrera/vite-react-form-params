84c638a135bf989f5eaf48fb40340fe56bcbc8f4
Rodrigo <rodrigo1cabrera2@gmail.com>
2023-08-31 19:04:27 -0300

    Use implicit return

--- src/features/common/components/Error.tsx
+++ src/features/common/components/Error.tsx
@@ -1,10 +1,8 @@
 import closeIcon from 'public/close.svg'
 
-export const Error = ({ error }: { error: string }) => {
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <img src={closeIcon} className="h-12 text-red-300" />
-      <p className="text-lg text-indigo-400">{error}</p>
-    </div>
-  )
-}
+export const Error = ({ error }: { error: string }) => (
+  <div className="flex flex-col items-center justify-center gap-4">
+    <img src={closeIcon} className="h-12 text-red-300" />
+    <p className="text-lg text-indigo-400">{error}</p>
+  </div>
+)
